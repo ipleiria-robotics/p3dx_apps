bd755e02e610826777c8b68c52aebb66ef8f5ecc
Hugo Costelha <hugo.costelha@ipleiria.pt>
2015-03-02 16:02:39 +0000

    Corrected launch script

--- scripts/launch_basic.sh
+++ scripts/launch_basic.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-roslaunch p3dx_apps p3dx_real.launch
+source /opt/ros/indigo/setup.bash
+source ~/ros/devel/setup.sh
+/opt/ros/indigo/bin/roslaunch p3dx_apps p3dx_real.launch
